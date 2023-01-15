ff5b924e2d9737cbe193ccae73201bb5a3cd2974
DmitriiDobr <72938753+DmitriiDobr@users.noreply.github.com>
2023-01-15 15:57:57 +0300

    first_task_threads

---
+++ first_theme/first_max_interval/Main.java
@@ -0,0 +1,70 @@
+package first_theme.first_max_interval;//package Threads;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        String[] texts = new String[25];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("aab", 30_000);
+        }
+
+        long startTs = System.currentTimeMillis(); // start time
+        List<Future<Integer>> lstOfFuture = new ArrayList<>();
+        final ExecutorService threadPool = Executors.newFixedThreadPool(texts.length);
+        for (String text : texts) {
+
+            Callable<Integer> logic = ()->{
+                int maxSize = 0;
+                for (int i = 0; i < text.length(); i++) {
+                    for (int j = 0; j < text.length(); j++) {
+                        if (i >= j) {
+                            continue;
+                        }
+                        boolean bFound = false;
+                        for (int k = i; k < j; k++) {
+                            if (text.charAt(k) == 'b') {
+                                bFound = true;
+                                break;
+                            }
+                        }
+                        if (!bFound && maxSize < j - i) {
+                            maxSize = j - i;
+                        }
+                    }
+                }
+                System.out.println(text.substring(0, 50) + " -> " + maxSize);
+                return maxSize;
+
+            };
+            final Future<Integer> task = threadPool.submit(logic);
+            lstOfFuture.add(task);
+        }
+        threadPool.shutdown();
+        int currentMax = 0;
+        for (Future<Integer> integerFuture : lstOfFuture) {
+            if (integerFuture.get() > currentMax) {
+                currentMax = integerFuture.get();
+
+            }
+        }
+        long endTs = System.currentTimeMillis(); // end time
+
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+        System.out.println("Maximum " + currentMax);
+
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
